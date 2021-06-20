0c76cff7b8e008050e22fb9eae8e54b70f20edde
hoangvy1706 <75347118+hoangvy1706@users.noreply.github.com>
2021-06-20 12:56:50 +0700

    Upload DAT VE

--- src/redux/Reducer/DatVeReducer.js
+++ src/redux/Reducer/DatVeReducer.js
@@ -5,26 +5,25 @@ const stateDefault = {
 export const DatVeReducer = (state = stateDefault, action) => {
   switch (action.type) {
     case "CHON_GHE": {
-      // const index = state.danhSachGheDangDat.findIndex(
-      //   (ghe) => ghe.soGhe === action.ghe.soGhe
-      // );
-      // console.log(index);
-      // if (index === -1) {
-      //   state.danhSachGheDangDat.push(action.soGhe);
-      // }
-
-      // return { ...state };
-      state.danhSachGheDangDat.push(action.ghe.soGhe);
+      const index = state.danhSachGheDangDat.findIndex(
+        (ghe) =>  ghe.soGhe == action.ghe.soGhe
+      );
+      if (index == -1) {
+        state.danhSachGheDangDat.push(action.ghe);
+      } else {
+        state.danhSachGheDangDat.splice(index, 1);
+      }
+      return { ...state };
 
     }
     case "XOA_GHE": {
       const index = state.danhSachGheDangDat.findIndex(
-        (ghe) => ghe.soGhe === action.soGhe
+        (ghe) => ghe.soGhe == action.soGhe
       );
-      if (index !== -1) {
+      if (index != -1) {
         state.danhSachGheDangDat.splice(index, 1);
       }
-      return state;
+      return {...state};
     }
   }
   return { ...state };
